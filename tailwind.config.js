3d997d5a72357b334c36ae5b12d5d84d978e3602
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-06-08 12:30:01 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {},
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [],
+},
+module.exports = {
+  prefix: 'tw-',
+},
