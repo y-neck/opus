c6f619cc6d61d2c6783c180d78b8c27a33e9d4e5
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-06-12 14:25:12 +0200

    setup basic pages and layouts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"]
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+  app: {
+    head: {
+      title: '',
+      meta: [{ name: 'description', content: '' }],
+    },
+  },
 });
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+</template>
+
+<script setup>
+useHead({
+    title: '',
+    meta: [
+        { name: 'description', content: '' },
+    ],
+});
+
+
+</script>
+
+<style scoped>
+
+</style>
