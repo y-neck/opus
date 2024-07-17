b7e9234cd4f32890b225cb48a8bf5a00d9146d2d
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-07-17 14:57:24 +0200

    nuxt reset to default

---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  title: '',
+  description: '',
+  layout: 'default',
+});
+</script>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
-
-  app: {
-    head: {
-      title: '',
-      meta: [{ name: 'description', content: '' }],
-    },
-  },
-
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-    redirect: false, // Enable if you want to redirect to login automatically; http://supabase.nuxtjs.org/get-started#redirect
-    redirectOptions: {
-      login: '/login', // Page to redirect the user to
-      callback: '/confirm', // Page to redirect the user to after login
-      include: undefined,
-      exclude: [],
-      cookieRedirect: false,
-    },
-  },
-
-  compatibilityDate: '2024-07-15',
-});
+  modules: ["@nuxtjs/tailwindcss"]
+})
