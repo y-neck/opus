7c968da5766ad24242d42ab5c3c7089798fd605f
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-07-17 15:16:03 +0200

    add supabase pkg and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
-})
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false, // Enable if you want to redirect to login automatically; http://supabase.nuxtjs.org/get-started#redirect
+    redirectOptions: {
+      login: '/login', // Page to redirect the user to
+      callback: '/confirm', // Page to redirect the user to after login
+      include: undefined,
+      exclude: [],
+      cookieRedirect: false,
+    },
+  },
+});
