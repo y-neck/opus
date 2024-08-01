75c23fb8194971dd1b423d2fddca828abd10e2f5
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-08-01 15:51:14 +0200

    viewport config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,15 @@ export default defineNuxtConfig({
       cookieRedirect: false,
     },
   },
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1',
+        },
+        { charset: 'utf-8' },
+      ],
+    },
+  },
 });
