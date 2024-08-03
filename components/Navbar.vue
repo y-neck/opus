4f980faf691124150c75e01ab052c2256e37e809
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-08-03 12:54:48 +0200

    navbar for mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav
-      class="md:w-[266px] md:h-screen flex md:flex-col gap-8 border-r-2 border-grey-100"
+      class="md:w-[266px] md:h-screen flex md:flex-col gap-8 md:border-r-2 border-grey-100 max-sm:w-screen max-sm:fixed max-sm:top-[95vh] max-sm:border-t-2"
     >
       <div id="main-nav" class="hidden md:flex flex-col">
         <NuxtLink to="/inbox" class="nav-item">
@@ -17,8 +17,10 @@
           <p class="nav-item-text">Settings</p></NuxtLink
         >
       </div>
-      <div id="team-nav" class="flex flex-col">
-        <p id="team-name" class="text-grey-500 text-sm">[Team Name]</p>
+      <div id="team-nav" class="flex md:flex-col justify-between max-sm:gap-8">
+        <p id="team-name" class="hidden md:block text-grey-500 text-sm">
+          [Team Name]
+        </p>
         <NuxtLink to="/tasks" class="nav-item">
           <img src="" alt="icn" />
           <p class="nav-item-text">Tasks</p></NuxtLink
@@ -57,4 +59,9 @@
   padding-inline: 8px;
 }
 
+@media (max-width: 640px) {
+  .nav-item-text {
+    display: none;
+  }
+}
 </style>
