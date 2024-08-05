c23d7edc9087a2156ecebb48ca3fb0781049136e
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-08-05 10:07:33 +0200

    login page added

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col">
+      <input type="email">
+      <input type="password">
+      <button>Login</button>
+      <p>Forgotten your password?</p>
+      <p>Create Account</p>
+      <InboxIcon />
+    </div>
+  </template>
+  
+  <script setup>
+  
+  definePageMeta({
+    title: 'Home',
+    description: '',
+    layout: false,
+  });
+  </script>
+  
+  <style scoped></style>
