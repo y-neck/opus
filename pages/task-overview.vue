0f9b78bc582c20fa71da4e039230bbbabc04cd1d
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-08-16 13:59:04 +0200

    /task-overview page structure (wip)

---
+++ pages/task-overview.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flex flex-col w-full">
+    <Header pageTitle="Task Overview" pageIcon="NotepadIcon" />
+    <main class="p-16">
+      <div v-for="project in projectTasks">
+        <div id="project-tasks-container w-full pb-16">
+          <div
+            class="project-title flex items-center gap-4 border-b-2 border-b-grey-100"
+          >
+            <h1 class="project-title text-2xl font-medium text-grey-500">
+              {{ project.projectName }}
+            </h1>
+            <p
+              class="project-task-count text-grey-700 text-xs font-semibold p-1 w-4 bg-grey-100 rounded"
+            >
+              {{ project.tasks.length }}
+            </p>
+            <!-- TODO: Add task counter -->
+          </div>
+          <div class="taskList" v-for="task in project.tasks">
+            <div class="task py-4 border-b-2 border-b-grey-100">
+              <span class="task-name text-grey-950">{{ task }}</span>
+              <div class="task-properties flex gap-1 text-grey-500">
+                <span class="task-property-due">Due Date</span>
+                <span class="divider">·</span>
+                <span class="task-property-project">
+                  {{ project.projectName }}
+                </span>
+                <span class="divider">·</span
+                ><span class="task-property-assignees">N/A</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+// Supabase auth
+const loading = ref(true);
+loading.value = true;
+const user = useSupabaseUser();
+
+console.log(user);
+
+import Header from '~/components/Header.vue';
+
+//Db logic
+const projectTasks = ref([
+  { projectName: 'Urban Wildlife', tasks: ['task1', 'task2', 'task3'] },
+  { projectName: 'Campus Navigation', tasks: ['task4', 'task5'] },
+]); /* TODO: Replace with actual data from DB */
+
+//Page meta
+definePageMeta({
+  title: 'Task Overview',
+  description: '',
+  layout: 'default',
+});
+</script>
+
+<style scoped></style>
