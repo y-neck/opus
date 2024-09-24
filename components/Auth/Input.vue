4d33a1a6ee46ffde7e8ce7dda0454d2e0bd07cf8
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-09-24 09:02:48 +0200

    Change in styling

--- components/Auth/Input.vue
+++ components/Auth/Input.vue
@@ -2,7 +2,7 @@
 import type { Title } from '#build/components';
 <template>
     <label class="text-sm text-grey-500 mb-1" :for="type"> {{ label }} </label>
-    <input class=" w-[22.125rem] h-10 border text-grey-700 border-grey-100 rounded-lg pl-3 placeholder-grey-400 pr-2 mb-2
+    <input class=" w-[20rem] h-10 border text-grey-700 border-grey-100 rounded-lg pl-3 placeholder-grey-400 pr-2 mb-2
         focus:outline-none focus:border-grey-200" :type="type" :placeholder="placeholder" :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)" :pattern="pattern" required />
 </template>
