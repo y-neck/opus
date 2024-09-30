b03e9705cdc9360b5fb971142c339fc162272048
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-09-30 12:36:34 +0200

    styling; follow-up on https://github.com/y-neck/opus/commit/7d665e7a99970bee26480d1e3cc95676d1670c3f

--- components/DropdownMenu.vue
+++ components/DropdownMenu.vue
@@ -14,9 +14,13 @@ const isVisible = ref(false);
 <style scoped>
 .dropdown-overlay {
   display: flex; /* Ensure it uses flexbox for alignment */
+  flex-direction: column;
   justify-content: flex-start; /* Align items to the start */
   align-items: flex-start; /* Align items to the start */
   z-index: 1000; /* Ensure it covers other elements */
+  gap: 0.5rem; /* Add gap between items for vertical alignment */
+  border-radius: 0.5rem /* 8px */;
+  width: fit-content;
 }
 
 .dropdown-menu {
@@ -26,11 +30,12 @@ const isVisible = ref(false);
   min-width: 10rem;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   z-index: 10;
+  padding: 8px;
+  border-radius: 0.5rem /* 8px */;
 }
 
 .dropdown-item {
   color: black;
-  padding: 12px 16px;
   text-decoration: none;
   display: block;
 }
