84ba2db25a043c781f994f54e02239f6d4277ed6
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-09-30 16:52:38 +0200

    slot added to add additional actions to pages

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,18 @@
 <template>
   <div
     id="page-description"
-    class="px-6 pt-2 pb-4 border-b-2 border-grey-100 text-grey-500 flex items-center gap-1"
+    class="px-6 pt-2 pb-4 border-b-2 border-grey-100 text-grey-500 flex items-center justify-between"
   >
-    <component :is="pageIcon" class="inline" />
-    <!-- Dynamic page icon -->
-    <h1>{{ pageTitle }}</h1>
+    <div class="flex items-center gap-1">
+      <component :is="pageIcon" class="inline" />
+      <!-- Dynamic page icon -->
+      <h1>{{ pageTitle }}</h1>
+    </div>
+
+    <!-- Slot for additional content -->
+    <div>
+      <slot name="actions"></slot>
+    </div>
   </div>
 </template>
 
