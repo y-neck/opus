b26662f4fee058a2e587295b99710199e6c47dde
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-10-01 10:19:54 +0200

    add timeline page

---
+++ pages/timeline.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-col w-full">
+      <Header pageTitle="Index" pageIcon="LayoutGridIcon" />
+      <main class="p-16">
+        <p>Hello world</p>
+      </main>
+    </div>
+  </template>
+  
+  <script setup>
+  // Supabase auth
+  const loading = ref(true);
+  loading.value = true;
+  const user = useSupabaseUser();
+  
+  console.log(user);
+  
+  import Header from '~/components/Header.vue';
+  
+  //Page meta
+  definePageMeta({
+    title: 'Home',
+    description: '',
+    layout: 'default',
+  });
+  </script>
+  
+  <style scoped></style>
+  
