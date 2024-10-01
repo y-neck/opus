e557f04406a2e7e0aeea8ad0a4794a35644a9728
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-10-01 17:51:52 +0200

    copy group stylings from https://github.com/y-neck/opus/blob/9a1d56bbd66666096ac7bb42d90933bb6e3511a6/pages/documents.vue#L43-L60

--- pages/tasks.vue
+++ pages/tasks.vue
@@ -28,38 +28,46 @@
             <div
               v-for="(task, taskIndex) in section.tasks"
               :key="task.id"
-              class="task-container flex items-center align-top gap-2 p-4 h-16 w-full border-b-2 border-b-grey-100"
+              class="task-container group flex items-center justify-between gap-2 p-4 h-16 w-full border-b-2 border-b-grey-100"
             >
-              <input
-                class="task-checkbox w-4 h-4 border-grey-700"
-                type="checkbox"
-                :name="`check-task-${task.id}`"
-                :id="`task-${task.id}`"
-              />
-              <div class="task-info">
-                <p class="task-property-title" :id="`task-${task.name}`">
-                  {{ task.name }}
-                </p>
-                <div class="task-property-description flex text-grey-500 gap-1">
-                  <p class="task-property-dueDate">
-                    {{
-                      `${task.dueDate.day}.${task.dueDate.month}.${task.dueDate.year}` ||
-                      'NaN'
-                    }}
+              <div
+                class="task-info flex flex-row gap-2 align-baseline items-center"
+              >
+                <input
+                  class="task-checkbox w-4 h-4 border-grey-700"
+                  type="checkbox"
+                  :name="`check-task-${task.id}`"
+                  :id="`task-${task.id}`"
+                />
+                <div class="task-properties">
+                  <p class="task-property-title" :id="`task-${task.name}`">
+                    {{ task.name }}
                   </p>
-                  <span class="divider">·</span>
-                  <p class="task-property-assignee">
-                    {{
-                      task.assignedTo
-                        .map((member: any) => member.name)
-                        .join(', ') || 'Everyone'
-                    }}
-                  </p>
+                  <div
+                    class="task-property-description flex text-grey-500 gap-1"
+                  >
+                    <p class="task-property-dueDate">
+                      {{
+                        `${task.dueDate.day}.${task.dueDate.month}.${task.dueDate.year}` ||
+                        'NaN'
+                      }}
+                    </p>
+                    <span class="divider">·</span>
+                    <p class="task-property-assignee">
+                      {{
+                        task.assignedTo
+                          .map((member: any) => member.name)
+                          .join(', ') || 'Everyone'
+                      }}
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div class="task-edit-actions flex justify-self-end gap-1">
+              <div
+                class="task-edit-actions flex flex-row opacity-0 gap-1 mt-1 group-hover:opacity-100 transition"
+              >
                 <button
-                  class="edit-task-btn"
+                  class="edit-task-btn w-5 h-5 flex justify-center items-center rounded cursor-pointer hover:bg-grey-100 active:text-grey-950 text-grey-500 transition"
                   :data-task-id="task.id"
                   @click="toggleDropdown(`edit-task-dd-${task.id}`)"
                 >
@@ -85,7 +93,7 @@
                   </p>
                 </DropdownMenu>
                 <button
-                  class="context-menu-btn"
+                  class="context-menu-btn w-5 h-5 flex justify-center items-center rounded cursor-pointer hover:bg-grey-100 active:text-grey-950 text-grey-500 transition"
                   :data-task-id="task.id"
                   @click="toggleDropdown(`context-menu-dd-${task.id}`)"
                 >
@@ -109,12 +117,112 @@
               </div>
             </div>
           </div>
+
           <button
             class="add-task-btn text-grey-500"
             @click="openNewTaskWindow()"
           >
             + Add Task
           </button>
+          <div
+            id="add-task-backdrop"
+            class="fixed inset-0 bg-black bg-opacity-25 hidden transition-all duration-300"
+          >
+            <div
+              id="add-task-window"
+              class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 min-w-fit h-fit bg-white flex flex-col gap-5 p-8 rounded-lg border-grey-100 border-2"
+            >
+              <h2 class="font-medium text-2xl">Add a new task</h2>
+              <form id="new-task-form" class="flex flex-col gap-2">
+                <label for="new-task-name" class="new-task-label">Task</label>
+                <input
+                  id="new-task-name"
+                  class="w-full h-12 border border-grey-100 rounded-md px-3 placeholder:text-grey-400"
+                  type="text"
+                  placeholder="Task name"
+                  required
+                />
+                <div id="new-task-properties" class="flex gap-2">
+                  <label for="new-task-section" class="sr-only"
+                    >Select section</label
+                  >
+                  <select
+                    id="new-task-section"
+                    class="w-full h-12 border border-grey-100 rounded-md px-3 bg-white"
+                    required
+                  >
+                    <option value="" selected disabled hidden>
+                      Select a section
+                    </option>
+                    <option
+                      v-for="section in project.taskSections"
+                      :key="section.index"
+                      :value="section.name"
+                      class=""
+                    >
+                      {{ section.name }}
+                    </option>
+                  </select>
+                  <label for="new-task-due-date" class="sr-only"
+                    >Due Date</label
+                  >
+                  <input
+                    id="new-task-due-date"
+                    class="h-12 border border-grey-100 rounded-md px-3 placeholder:text-grey-400 w-fit"
+                    type="date"
+                    placeholder="Due Date"
+                    required
+                  />
+                </div>
+                <label for="new-task-assignees" class="new-task-label"
+                  >Assign to</label
+                >
+                <div class="new-task-assignees flex gap-2 flex-wrap">
+                  <div
+                    v-for="member in project.projectMembers"
+                    :key="member.id"
+                    class="new-task-assignee flex items-center justify-center gap-2 border border-grey-100 rounded-md px-3 py-2 text-grey-700 hover:bg-grey-100 transition"
+                  >
+                    <label
+                      :for="'member-' + member.id"
+                      class="flex items-center justify-center gap-2"
+                    >
+                      {{ member.name }}
+                    </label>
+                    <input
+                      type="checkbox"
+                      :id="'member-' + member.id"
+                      :value="member.name"
+                      class="rounded-full"
+                    />
+                  </div>
+                </div>
+                <div id="new-task-error" class="hidden">
+                  <p id="new-task-error-msg" class="text-destructiveRed">
+                    Please fill in at least name and due date
+                  </p>
+                </div>
+              </form>
+              <div id="add-task-window-btns" class="flex gap-2 self-end">
+                <button
+                  id="close-new-task-window"
+                  class="w-fit h-12 hover:rounded-md px-3 hover:bg-grey-100 transition"
+                  type="button"
+                  @click="closeNewTaskWindow()"
+                >
+                  Cancel
+                </button>
+                <button
+                  id="add-new-task"
+                  class="w-fit h-12 rounded-md px-3 text-grey-700 bg-grey-100 hover:bg-grey-800 hover:text-grey-50 transition"
+                  type="button"
+                  @click="addNewTask()"
+                >
+                  Create
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
@@ -156,6 +264,21 @@ function openNewTaskWindow() {
     addNewTaskWindow.classList.toggle('hidden');
   }
 }
+
+// Add new task
+function addNewTask() {
+  console.log('add new task');
+}
+
+// Close new-task-window
+function closeNewTaskWindow() {
+  const addNewTaskWindow =
+    document.querySelector<HTMLDivElement>('#add-task-backdrop');
+  if (addNewTaskWindow) {
+    addNewTaskWindow.classList.add('hidden');
+  }
+}
+
 // Close new-task-window on escape-key
 onMounted(() => {
   window.addEventListener('keydown', (e) => {
