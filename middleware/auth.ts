03c4556c355fe897f229fce01569e69a35cdc90a
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-11-07 00:00:19 +0100

    add function to get user from Profile table as well as from auth

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,18 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware(async () => {
   const user = useSupabaseUser();
+  // DEBUG:
+  console.log(user.value);
 
   if (!user.value) {
     return navigateTo('/login');
   }
+
+  // Get user from Profile table
+  const { data: userProfile, error: userProfileError } =
+    await supabaseConnection()
+      .supabase.from('Profiles')
+      .select('*')
+      .eq('user_id', user.value.id);
+      // DEBUG:
+      console.log('User profile:', userProfile);
 });
