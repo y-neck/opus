a9eb25a69c652a61f50f93d61d4c93b2bcd01153
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-12-10 15:42:27 +0100

    ProjectSwitch styling and dropdown functionality

--- components/ProjectSwitch.vue
+++ components/ProjectSwitch.vue
@@ -1,16 +1,29 @@
 <template>
-    <DropdownMenu id="project-dd">
-                  <p>Select Project</p>
-                  <p 
-                  v-for="project in projectTasks" 
-                  :key="project.projectId" 
-                  role="menuitem" 
-                  :class="['dropdown-menu-item', { active: project.projectId === projectStore.activeProjectId }]"
-                  @click="setActiveProject(project.projectId as number)"
-                  >
-                    {{ project.projectName }}
-                  </p>
-            </DropdownMenu>
+  <div>
+    <button 
+      id="project-dd" 
+      class="dropdown-menu-button flex items-center justify-between px-4 py-2 border-2 border-grey-100 rounded-md" 
+      @click="toggleDropdown('project-dd')">
+      <span class="chevron">▼</span>
+      <span>{{ activeProjectName || 'Select Project' }}</span>
+    </button>
+  <!-- Dropdown Options -->
+    <DropdownMenu 
+      id="project-dd-container"
+      v-show="isDropdownVisible['project-dd']"
+      class="dropdown-menu-options">
+      <p 
+        v-for="project in projectTasks" 
+        :key="project.projectId" 
+        role="menuitem" 
+        :class="['dropdown-menu-item px-1 truncate', { active: project.projectId === projectStore.activeProjectId }]"
+        @click="setActiveProject(project.projectId as number, project.projectName as string)"
+        >
+        
+          {{ project.projectName }}
+      </p>
+    </DropdownMenu>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,11 +38,13 @@ import { getProjects } from '~/middleware/projectMiddleware';
 const projectStore=useProjectStore();
 // Use pinia store state and actions, destructure reactive properties from store
 const {activeProjectId} = projectStore;
-// Set active project
-const setActiveProject = (projectId: number) => {
-  projectStore.setActiveProject(projectId);
+const activeProjectName = computed(() => projectStore.activeProjectName);
+// Set active project in store
+const setActiveProject = (projectId: number, projectName: string) => {
+  projectStore.setActiveProject(projectId, projectName);
   // // DEBUG:
   // console.log(`Set active project: ${activeProjectId}`);
+  toggleDropdown('project-dd');
 };
 
 // Watch for changes in active project
@@ -54,14 +69,6 @@ onMounted(async () => {
     }
 });
 
-// // Use pinia store state and actions
-// const activeProjectId = projectStore.activeProjectId;
-// const setActiveProject = (projectId: number) => {
-//   projectStore.setActiveProject(projectId);
-//   // DEBUG:
-//   console.log(`Set active project: ${activeProjectId}`);
-// };
-
 /* Handle dropdown */
 import {
   isDropdownVisible,
@@ -70,13 +77,16 @@ import {
 </script>
 
 <style scoped>
-.dropdown-menu-item {
-  @apply flex items-center gap-2;
+.dropdown-menu{
+  @apply border-grey-100 border-2 rounded-md;
+}
+.dropdown-menu-item :not(:first){
+  @apply flex items-center;
 }
 .dropdown-menu-item:hover {
-  @apply cursor-pointer bg-grey-100;
+  @apply cursor-pointer bg-grey-100 rounded-md;
 }
 .active {
-  @apply bg-grey-200; /* Highlight active project */
+  @apply bg-grey-200 rounded-md; /* Highlight active project */
 }
 </style>
--- middleware/projectStore.ts
+++ middleware/projectStore.ts
@@ -3,15 +3,17 @@ import {defineStore} from 'pinia';
 export const useProjectStore = defineStore('projectStore', {
     state: () => ({
         activeProjectId: null as number | null, // Active project ID
+        activeProjectName: '' as string // Active project name
     }),
     actions: {
-        setActiveProject(projectId: number) {
-            // DEBUG:
-            console.log(`Setting active project to: ${projectId}`);
+        setActiveProject(projectId: number, projectName: string) {
+            // // DEBUG:
+            // console.log(`Setting active project to: ${projectId}`);
             // Set the active project ID
             this.activeProjectId = projectId;
+            this.activeProjectName = projectName;
             // DEBUG:
-            console.log(`New active project ID: ${this.activeProjectId}`);
+            console.log(`New active project ID: ${this.activeProjectId}, ${this.activeProjectName}`);
         }
     }
 })
