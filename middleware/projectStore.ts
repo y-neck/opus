6c67ae0b31f1da399ea90bf6fbe128c36a8a01f7
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-12-11 10:20:53 +0100

    ProjectSwitch global function

--- middleware/projectStore.ts
+++ middleware/projectStore.ts
@@ -1,9 +1,12 @@
+import type { Project } from './models/projectsETL';
+import { getProjects } from './projectMiddleware';
 import {defineStore} from 'pinia';
 
 export const useProjectStore = defineStore('projectStore', {
     state: () => ({
         activeProjectId: null as number | null, // Active project ID
-        activeProjectName: '' as string // Active project name
+        activeProjectName: '' as string, // Active project name
+        projects: [] as Project[],
     }),
     actions: {
         setActiveProject(projectId: number, projectName: string) {
@@ -14,6 +17,12 @@ export const useProjectStore = defineStore('projectStore', {
             this.activeProjectName = projectName;
             // DEBUG:
             console.log(`New active project ID: ${this.activeProjectId}, ${this.activeProjectName}`);
+        },
+            // Fetch projects
+            async fetchProjects() {
+                const projects = await getProjects();
+                this.projects = projects;
+            }
         }
     }
-})
+)
