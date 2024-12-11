a5c0028c554362bd7d930e327898f328251bb385
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-12-11 10:35:45 +0100

    /team comments

---
+++ pages/team.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "", /* Page title */
+  ogTitle: "", /* Page title without branding */
+  ogSiteName: "", /* Overall site name */
+  ogType: "", /* 'website' | 'article' | 'book' | 'profile' */
+  description: "",
+  ogDescription: "",
+  creator: "",
+  robots: "", /* 'index' | 'follow' | 'noindex' | 'nofollow' */
+  ogImage: "" /* Image for sharing */
+})
+
+/* INFO:
+Active project can be retrieved as follows:
+https://github.com/y-neck/opus/blob/942826efde2f4d58ee900953ffbea96c0f4c3321/pages/tasks.vue#L273-L281
+*/
+
+</script>
+
+<style scoped>
+</style>
