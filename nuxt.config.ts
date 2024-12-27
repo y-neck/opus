c9025e0ad9baf03a45175e1f1715e28800b0f44b
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-27 16:28:46 +0100

    fix: add correct path for stylesheet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,37 +2,42 @@
 export default defineNuxtConfig({
   components: {
     global: true,
-    dirs: ['~/components/icons', '~/components'],
+    dirs: ["~/components/icons", "~/components"],
   },
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@nuxt/image', '@pinia/nuxt'],
-  plugins: ['~/plugins/preamble.client.ts'],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/supabase",
+    "@nuxt/image",
+    "@pinia/nuxt",
+  ],
+  plugins: ["~/plugins/preamble.client.ts"],
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     redirect: true, // INFO: Enable if you want to redirect to login automatically; http://supabase.nuxtjs.org/get-started#redirect
     cookieOptions: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === "production",
     },
     redirectOptions: {
-      login: '/login', // Page to redirect the user to
-      callback: '/confirm', // Page to redirect the user to after login
+      login: "/login", // Page to redirect the user to
+      callback: "/confirm", // Page to redirect the user to after login
       include: undefined,
-      exclude: ['/register', '/password-recovery', '/update-password'],
+      exclude: ["/register", "/password-recovery", "/update-password"],
       cookieRedirect: false,
     },
   },
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: './public/favicon.ico' },
-        { rel: 'stylesheet', href: './src/styles.css' },
+        { rel: "icon", type: "image/x-icon", href: "./public/favicon.ico" },
+        { rel: "stylesheet", href: "/styles.css" },
       ],
       htmlAttrs: {
-        lang: 'en',
+        lang: "en",
       },
     },
   },
