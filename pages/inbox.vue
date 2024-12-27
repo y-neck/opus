c60a6f997f9ce0cafccec43a17ecb8c7f23ebfc1
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-27 16:35:40 +0100

    feat: Add inbox page

---
+++ pages/inbox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col w-full">
+    <Header pageTitle="Index" pageIcon="LayoutGridIcon" />
+    <main class="p-16"></main>
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "" /* Page title */,
+  ogTitle: "" /* Page title without branding */,
+  ogSiteName: "" /* Overall site name */,
+  ogType: "website" /* 'website' | 'article' | 'book' | 'profile' */,
+  description: "",
+  ogDescription: "",
+  creator: "",
+  robots: "" /* 'index' | 'follow' | 'noindex' | 'nofollow' */,
+  ogImage: "" /* Image for sharing */,
+});
+</script>
+
+<style scoped></style>
