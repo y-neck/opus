a897c6e7da35602964191e0b86f85deb82e59c89
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-28 08:59:48 +0100

    refactor: Simplify structure and remove unused styles

--- components/DropdownMenu.vue
+++ components/DropdownMenu.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="dropdown-overlay" :id="props.id" :class="props.class">
-    <div class="dropdown-menu" role="menu" aria-label="Dropdown Menu">
+  <div :id="props.id" :class="props.class">
+    <div
+      class="flex flex-col gap-2 p-2 pl-4 w-[14rem] border border-grey-100 absolute bg-white rounded-lg"
+      role="menu"
+      aria-label="Dropdown Menu"
+    >
       <slot></slot>
     </div>
   </div>
@@ -8,35 +12,4 @@
 
 <script setup>
 const props = defineProps({ id: String, class: String });
-const isVisible = ref(false);
 </script>
-
-<style scoped>
-.dropdown-overlay {
-  display: flex; /* Ensure it uses flexbox for alignment */
-  flex-direction: column !important;
-  justify-content: flex-start; /* Align items to the start */
-  align-items: flex-start; /* Align items to the start */
-  z-index: 1000; /* Ensure it covers other elements */
-  gap: 0.5rem; /* Add gap between items for vertical alignment */
-  border-radius: 0.5rem /* 8px */;
-  width: fit-content;
-}
-
-.dropdown-menu {
-  display: block;
-  position: absolute;
-  @apply bg-white;
-  min-width: 10rem;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  z-index: 10;
-  padding: 8px;
-  border-radius: 0.5rem /* 8px */;
-}
-
-.dropdown-item {
-  color: black;
-  text-decoration: none;
-  display: block;
-}
-</style>
