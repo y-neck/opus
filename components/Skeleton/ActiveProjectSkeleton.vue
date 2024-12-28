918c6c7317a2c1af5735730a194a14aa3be44be2
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-28 09:02:21 +0100

    feat: Add skeleton components for loading states in various sections

---
+++ components/Skeleton/ActiveProjectSkeleton.vue
@@ -0,0 +1,6 @@
+<template>
+  <div role="status" class="flex flex-col max-w-sm animate-pulse">
+    <div class="h-2 bg-grey-200 rounded-full w-20 mb-1 mt-2"></div>
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
