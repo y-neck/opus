b62db84d809930a4f277f0639c1bec0245e3e467
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-29 22:30:33 +0100

    refactor: Remove unused CSS styles from default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,17 +8,3 @@
 </template>
 
 <script setup></script>
-
-<style scoped>
-/* :root {
-  --white: #fff;
-  --text: #000;
-}
-
-div {
-  background-color: --white;
-  color: var(--text);
-  padding: 16px;
-  height: 100vh;
-} */
-</style>
