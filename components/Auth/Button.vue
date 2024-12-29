5ce2759ba0f2cf00664242c79d40a9dc540887a5
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-29 22:27:38 +0100

    feat: Enhance button component with loading state and updated styles

--- components/Auth/Button.vue
+++ components/Auth/Button.vue
@@ -1,16 +1,26 @@
 <template>
   <button
     :disabled="disabled"
-    class="w-[20rem] h-10 bg-grey-700 active:hover:bg-grey-800 text-grey-50 rounded-lg font-regular transition disabled:opacity-80 disabled:transition mt-4 font-medium"
+    class="w-[20rem] h-10 bg-grey-700 hover:bg-grey-800 text-grey-50 rounded-lg font-regular transition disabled:bg-grey-100 disabled:text-grey-300 disabled:transition mt-2 font-medium"
     type="submit"
   >
-    {{ title }}
+    <div v-if="loading" class="flex justify-center items-center">
+      <VueSpinnerIos size="20" color="#fafafa" stroke-width="20px" />
+    </div>
+    <span v-else>
+      {{ label }}
+    </span>
   </button>
 </template>
 
 <script setup>
+import { VueSpinnerIos } from "vue3-spinners";
+
 defineProps({
-  title: String,
+  label: String,
   disabled: Boolean,
+  loading: Boolean,
 });
 </script>
+
+<style scoped></style>
