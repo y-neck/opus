5092b379988a9c0ce95740d0d3455a1a723078d2
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-29 22:27:02 +0100

    fix: Update component to fetch image from Profiles table

--- components/ProfilePicture.vue
+++ components/ProfilePicture.vue
@@ -2,11 +2,10 @@
   <div>
     <NuxtImg
       id="profile-img"
-      :src="src"
+      :src="ImageSource"
       class="rounded-full bg-grey-100"
-      width="64"
-      height="64"
-      :placeholder="[64, 64]"
+      width="96"
+      height="96"
       fit="cover"
       alt="Profile Picture"
       loading="lazy"
@@ -14,41 +13,24 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { supabaseConnection } from '~/composables/supabaseConnection';
-import { onMounted } from 'vue';
-let user = supabaseConnection().user.value;
+<script setup>
+import { onMounted } from "vue";
 
-const src = ref<string>('');
+let user = supabaseConnection().user.value;
+const ImageSource = ref("");
 
 // Fetch the profile picture URL when the component mounts
 onMounted(async () => {
   if (user) {
     // List all files in the user's folder
-    const { data: files, error } = await supabaseConnection()
-      .supabase.storage.from('profile_img')
-      .list(user.id); // Directly using `user.id`
+    const { data, error } = await supabaseConnection()
+      .supabase.from("Profiles")
+      .select("profile_img, user_id")
+      .eq("user_id", user.id);
 
-    if (error) {
-      console.error('Error listing files:', error);
-      src.value = '~/assets/default-profile.jpg';
-      return;
-    }
-
-    if (files && files.length > 0) {
-      // Choose the first file found (or apply additional logic if needed)
-      const profileImageFilename = files[0].name;
-
-      // Fetch the public URL of the selected file
-      const { data: fileUrl } = supabaseConnection()
-        .supabase.storage.from('profile_img')
-        .getPublicUrl(`${user.id}/${profileImageFilename}`);
-
-      src.value = fileUrl?.publicUrl || '~/assets/default-profile.jpg';
-    } else {
-      // Set default image if no files are found
-      src.value = '~/assets/default-profile.jpg';
-    }
+    ImageSource.value = `https://zdrhwehycbxujrbltjlj.supabase.co/storage/v1/object/public/profile_img/${
+      user.id
+    }/${data[0].profile_img}?v=${Date.now()}`;
   }
 });
 </script>
