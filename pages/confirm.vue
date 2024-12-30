9231c177e55481a7f0f67834975a94b81aa2d1ba
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2024-12-30 08:41:31 +0100

    fix: Remove unnecessary class from confirmation message

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -16,7 +16,7 @@ definePageMeta({
   <div class="flex flex-col items-center justify-center h-screen">
     <div class="text-left">
       <h2 class="text-2xl text-grey-700">Email Confirmed</h2>
-      <p class="text-grey-500 font-normal leading-[1.375rem] pt-0.5">
+      <p class="text-grey-500 font-normal pt-0.5">
         Thank you for confirming your email.<br />
         You will be redirected to the login page shortly.
       </p>
