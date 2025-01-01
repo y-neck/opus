f34da24533c44d4c15fdcb76bb9743e0386d538c
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-01 15:54:47 +0100

    fix: Adjust checkmark icon

--- components/icons/CheckmarkIcon.vue
+++ components/icons/CheckmarkIcon.vue
@@ -1,15 +1,15 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
+    width="13"
+    height="13"
     viewBox="0 0 24 24"
     fill="none"
   >
     <path
       d="M5 12.75L10 19L19 5"
       stroke="currentColor"
-      stroke-width="2.15"
+      stroke-width="3"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
