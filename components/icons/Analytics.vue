299c94c0b4eb579156ecb209a60c80ba2d30145a
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-05 12:07:20 +0100

    feat: Add new icon components for project icon feature

---
+++ components/icons/Analytics.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M3.75 12H7.75L10 16.25L14 7.75L16.25 12H20.25M5.75 20.25H18.25C19.3546 20.25 20.25 19.3546 20.25 18.25V5.75C20.25 4.64543 19.3546 3.75 18.25 3.75H5.75C4.64543 3.75 3.75 4.64543 3.75 5.75V18.25C3.75 19.3546 4.64543 20.25 5.75 20.25Z"
+      stroke="currentColor"
+      stroke-width="2.25"
+      stroke-linecap="square"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Analytics",
+};
+</script>
