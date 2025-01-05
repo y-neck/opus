c1e4f8bf5cb487dbc9c19c1a308534ccf9d707b0
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-05 12:08:57 +0100

    style: Update dropdown for improved layout and spacing

--- components/common/DropdownMenu.vue
+++ components/common/DropdownMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div :id="props.id" :class="props.class">
     <div
-      class="flex flex-col gap-2 p-2 pl-4 w-[14rem] border border-grey-100 absolute bg-white rounded-lg"
+      class="flex flex-col gap-2 p-2 pl-[7px] w-[15.5rem] border border-grey-100 absolute bg-white rounded-lg -ml-[0.75rem] mt-2"
       role="menu"
       aria-label="Dropdown Menu"
     >
