1af54a718aa36026a341342c229c99c46202efd3
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-05 15:44:28 +0100

    feat: Add new icons

---
+++ components/icons/Bean.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M19.2228 4.77723C22.414 7.96845 21.7672 13.7892 17.7782 17.7782C13.7892 21.7672 7.96845 22.414 4.77723 19.2228C1.58601 16.0315 2.23276 10.2108 6.22178 6.22178C10.2108 2.23276 16.0315 1.58601 19.2228 4.77723Z"
+      stroke="currentColor"
+      stroke-width="2.25"
+    />
+    <path
+      d="M4 18C8.12903 9.65217 15.871 14.3478 20 6"
+      stroke="currentColor"
+      stroke-width="2.25"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Bean",
+};
+</script>
