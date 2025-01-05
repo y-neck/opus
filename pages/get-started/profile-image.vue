4b3e729efffc79f790b23fe1af6581741112eb65
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-05 21:35:41 +0100

    style: Enhance profile image page layout and add user guidance

--- pages/get-started/profile-image.vue
+++ pages/get-started/profile-image.vue
@@ -1,8 +1,27 @@
 <template>
   <main>
-    <section class="flex flex-row justify-center items-center h-screen">
-      <ProfilePicture />
-      <UploadProfilePicture label="Upload Image" />
+    <section class="flex flex-col justify-center items-center h-screen">
+      <div class="text-left">
+        <h2 class="text-2xl text-grey-700 mb-5">Email Confirmed</h2>
+        <div class="flex items-end">
+          <ProfilePicture />
+          <UploadProfilePicture label="Upload Image" />
+        </div>
+        <div class="mt-5">
+          <p class="font-normal text-grey-500 text-sm w-[380px]">
+            Please dimensions of at least 200x200 pixels and a maximum size of
+            1MB. You can update your picture anytime in your account settings.
+          </p>
+        </div>
+        <div class="mt-10">
+          <NuxtLink
+            to="/task-overview"
+            class="flex flex-row items-center justify-center px-3 h-9 rounded-lg bg-grey-700 hover:bg-grey-800 text-grey-50 transition cursor-pointer w-[380px]"
+          >
+            Enter Profile
+          </NuxtLink>
+        </div>
+      </div>
     </section>
   </main>
 </template>
