c094a4e6b8134be3875fe96dc0e222b7acb5bd44
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-05 08:13:00 +0100

    fix: Refactor for improved project handling and navigation

--- components/layout/ProjectSwitch.vue
+++ components/layout/ProjectSwitch.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
+  <div v-if="activeProjectName">
     <button
       id="project-dd"
-      class="flex items-center justify-between gap-2 rounded-md"
+      class="flex items-center justify-between gap-2"
       @click="toggleDropdown('project-dd')"
     >
-      <!-- TODO: Replace icon with variable. -->
-      <span> <PlantIcon /></span>
-      <p v-if="activeProjectName">
-        {{ activeProjectName }}
-      </p>
-      <span v-else="DropdownSkeleton"> <DropdownSkeleton /> </span>
-      <span class="-ml-1"> <ChevronDownIcon /></span>
+      <span class="flex flex-row items-center gap-2" v-if="activeProjectName">
+        <p>
+          {{ activeProjectName }}
+        </p>
+
+        <span class="-ml-1"><ChevronDownIcon /></span>
+      </span>
+      <span v-else="DropdownSkeleton"><DropdownSkeleton /></span>
     </button>
-    <!-- Dropdown Options -->
     <DropdownMenu
       id="project-dd-container"
       v-show="isDropdownVisible['project-dd']"
@@ -36,9 +36,12 @@
         {{ project.projectName }}
       </p>
       <hr class="border-grey-100" />
-      <p class="flex flex-row items-center gap-2 cursor-pointer">
+      <NuxtLink
+        to="/project/create"
+        class="flex flex-row items-center gap-2 cursor-pointer"
+      >
         <span><PlusIcon /></span> Create Project
-      </p>
+      </NuxtLink>
       <p
         class="flex flex-row items-center gap-2 cursor-pointer text-destructive-red"
       >
@@ -46,11 +49,16 @@
       </p>
     </DropdownMenu>
   </div>
+  <div v-else>
+    <NuxtLink class="flex flex-row gap-2 items-center" to="/project/create"
+      ><span><PlusIcon /></span> Create Project</NuxtLink
+    >
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useProjectStore } from "~/store/project";
-import { getProjects } from "~/store/projectService";
+import { useProjectStore } from "~/middleware/store/project";
+import { getProjects } from "~/middleware/store/projectService";
 
 import { ref, onMounted } from "vue";
 import DropdownMenu from "~/components/common/DropdownMenu.vue";
@@ -66,11 +74,13 @@ interface Project {
 // Initialize pinia store
 const projectStore = useProjectStore();
 // Use pinia store state and actions, destructure reactive properties from store
-const { activeProjectId } = projectStore;
+
 const activeProjectName = computed(() => projectStore.activeProjectName);
 // Set active project in store
 const setActiveProject = (projectId: number, projectName: string) => {
   projectStore.setActiveProject(projectId, projectName);
+
+  console.log(activeProjectName);
 
   toggleDropdown("project-dd");
 };
