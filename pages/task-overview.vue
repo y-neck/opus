1e5679d046c6f4d0fcf043a6700f2882f86f843c
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-05 08:14:56 +0100

    feat: Add task overview with task list of all projects someone is in

--- pages/task-overview.vue
+++ pages/task-overview.vue
@@ -1,3 +1,16 @@
 <template>
-  <p>refactor</p>
+  <div class="flex flex-col w-full">
+    <Header page-title="Task Overview" page-icon="NotepadIcon" />
+    <OverviewList />
+  </div>
 </template>
+
+<script setup>
+import Header from "~/components/layout/Header.vue";
+import OverviewList from "~/components/project/overview/OverviewList.vue";
+
+definePageMeta({
+  middleware: "auth",
+  layout: "default",
+});
+</script>
