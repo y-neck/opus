0102e28adbecd7caa755f7ecf521ebfa4a799003
kevdercoder <119848827+kevdercoder@users.noreply.github.com>
2025-01-05 21:35:13 +0100

    refactor: Remove header and placeholder content from index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="flex flex-col w-full">
-    <Header pageTitle="Index" pageIcon="LayoutGridIcon" />
-    <main class="p-16">
-      <p>Hello world</p>
-    </main>
+    <main class="p-16"></main>
   </div>
 </template>
 
 <script setup>
-import Header from "~/components/layout/Header.vue";
-
 definePageMeta({
   layout: false,
 });
